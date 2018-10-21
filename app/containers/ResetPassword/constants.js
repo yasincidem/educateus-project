5129e8d01b1776ed0681e689c40379405cf1ec7e
serkanseker982 <seker.serkan.3@gmail.com>
2018-10-21 22:58:08 +0300

    resetpasswordcomp created

---
+++ app/containers/ResetPassword/constants.js
@@ -0,0 +1,3 @@
+export const SEND_PASSWORD_RESET_EMAIL = 'educateus/resetPassword/SEND_PASSWORD_RESET_EMAIL';
+export const EMAIL_ERROR = 'educateus/resetPassword/EMAIL_ERROR';
+export const CHANGE_EMAIL = 'boilerplate/Home/CHANGE_EMAIL';
